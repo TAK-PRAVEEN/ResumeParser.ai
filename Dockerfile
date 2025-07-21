38020e5b01c4cf3c5ef8a9128cb1cab0adb275fa
Praveen Tak <praveentak715@gmail.com>
2025-07-21 15:12:39 +0530

    app

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,13 @@ FROM python:3.10-slim
 
 WORKDIR /app
 
-COPY requirements.txt .
+COPY ../requirements.txt .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY . .
+COPY ../ .
 
 CMD ["python", "app/main.py"]
+
+
 
--- app/database/user_ops.py
+++ app/database/user_ops.py
@@ -1,25 +1,3 @@
-# from .db import db
-# import bcrypt
-
-# users = db["users"]
-
-# def get_user_by_email(email):
-#     if users.find_one({"email": email}):
-#         return True, "Email already exists"
-#     else:
-#         return False
-
-# def register_user(email, password):
-#     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-#     users.insert_one({"email": email, "password": hashed})
-#     return True, "User registered"
-
-# def validate_login(email, password):
-#     user = users.find_one({"email": email})
-#     if user and bcrypt.checkpw(password.encode(), user["password"]):
-#         return True
-#     return False
-
 from .db import db
 import bcrypt
 
