6ee55f7c715fb10bc0fb5bbc25bc0ce54825c6d6
Praveen Tak <praveentak715@gmail.com>
2025-07-24 13:37:15 +0530

    deploy

--- app/main.py
+++ app/main.py
@@ -165,4 +165,6 @@ def privacy():
     return render_template('PrivacyPolicy.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
