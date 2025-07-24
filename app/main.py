23530e70718d5c63b9a649a06682f9d49b5d6ede
Praveen Tak <praveentak715@gmail.com>
2025-07-24 11:59:20 +0530

    main.py

--- app/main.py
+++ app/main.py
@@ -165,4 +165,4 @@ def privacy():
     return render_template('PrivacyPolicy.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
