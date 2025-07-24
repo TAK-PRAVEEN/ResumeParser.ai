9f4bb135f2733f95493be79d20391e8332d356ff
Praveen Tak <praveentak715@gmail.com>
2025-07-24 13:21:40 +0530

    depoly

--- app/main.py
+++ app/main.py
@@ -163,3 +163,6 @@ def terms():
 @app.route('/privacy-policy')
 def privacy():
     return render_template('PrivacyPolicy.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
